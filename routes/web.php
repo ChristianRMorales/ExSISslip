0c7a7ba38923570485e12ccc672b6c185264b6a2
vincent123519 <113372799+vincent123519@users.noreply.github.com>
2023-08-22 16:00:46 +0800

    small changes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,37 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
+// Authentication routes
+Route::get('/login', 'AuthController@showLoginForm')->name('login');
+Route::post('/login', 'AuthController@login');
+Route::post('/logout', 'AuthController@logout')->name('logout');
+
+// Student routes
+Route::group(['middleware' => ['auth', 'role:student']], function () {
+    Route::get('/student/excuse-slip', 'StudentController@showExcuseSlipForm');
+    Route::post('/student/excuse-slip', 'StudentController@storeExcuseSlip');
+    Route::get('/student/feedback', 'StudentController@showFeedback');
+});
+
+// Counselor routes
+Route::group(['middleware' => ['auth', 'role:counselor']], function () {
+    Route::get('/counselor/excuse-slips', 'CounselorController@showExcuseSlips');
+    Route::get('/counselor/feedback', 'CounselorController@showFeedback');
+    Route::post('/counselor/feedback', 'CounselorController@provideFeedback');
+});
+
+// Dean routes
+Route::group(['middleware' => ['auth', 'role:dean']], function () {
+    Route::get('/dean/excuse-slips', 'DeanController@showExcuseSlips');
+    Route::post('/dean/approve', 'DeanController@approveExcuseSlip');
+    Route::post('/dean/reject', 'DeanController@rejectExcuseSlip');
+});
+
+// Teacher routes
+Route::group(['middleware' => ['auth', 'role:teacher']], function () {
+    Route::get('/teacher/excuse-slips', 'TeacherController@showExcuseSlips');
+    Route::post('/teacher/sign', 'TeacherController@signExcuseSlip');
+});
+
+// Common routes for all users
+Route::get('/dashboard', 'HomeController@index')->middleware('auth');
